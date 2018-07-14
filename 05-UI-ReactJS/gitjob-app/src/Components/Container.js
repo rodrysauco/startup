b579d2782b10fd109e9d2bc90afe0479d18b3cde
Matias Oliva <olivamati@gmail.com>
2018-07-14 17:49:00 -0300

    add axios dependency

--- 05-UI-ReactJS/gitjob-app/src/Components/Container.js
+++ 05-UI-ReactJS/gitjob-app/src/Components/Container.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class Container extends React.Component {
+    state = {
+        allJobs : [],
+        favJobs : []
+    }
 
+    /*componentDidMount() {
+        axios.get(`https://jobs.github.com/positions.json?description=python&location=new+york`,
+            {
+            headers: {
+                'Authorization' : '',
+                'Content-Type' : 'application/json'
+                }
+            })
+            .then(res => {
+                const jobs = res.data;
+                this.setState({ allJobs: jobs });
+        })
+    }*/
     render(){
-        return(<h4>Hola</h4>)
+        return(<h4>{this.state.allJobs}</h4>)
     }
 }
