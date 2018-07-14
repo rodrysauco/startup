336fd63d5b35021082e8a143439c9c8a97f75355
Matias Oliva <olivamati@gmail.com>
2018-07-14 17:11:52 -0300

    initial structure

---
+++ 05-UI-ReactJS/gitjob-app/src/Components/Container.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class Container extends React.Component {
+
+    render(){
+        return(<h4>Hola</h4>)
+    }
+}
---
+++ 05-UI-ReactJS/gitjob-app/src/Components/JobRow/JobRow.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default class JobRows extends React.Component {
+
+}
